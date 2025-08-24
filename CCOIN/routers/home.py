071c0e010c7705a3e5b5072fc58a95b301ec6d80
ccoin2025 <ccoin.ctg@gmail.com>
2025-08-24 21:41:56 +0330

    Update home.py

--- CCOIN/routers/home.py
+++ CCOIN/routers/home.py
@@ -12,7 +12,6 @@ import os
 router = APIRouter()
 limiter = Limiter(key_func=get_remote_address)
 router.state = type("State", (), {"limiter": limiter})()
-router.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "..", "templates"))
 
