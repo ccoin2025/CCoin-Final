8a20850f2d1ab4cb6c5c1f0a9cb4fec3f725d1fe
ccoin2025 <ccoin.ctg@gmail.com>
2025-09-02 01:05:02 +0330

    Update config.py

--- CCOIN/config.py
+++ CCOIN/config.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "CTG_COIN_BOT")  # اضافه کردن username Bot
 TELEGRAM_CHANNEL_USERNAME = os.getenv("TELEGRAM_CHANNEL_USERNAME")
 SOLANA_RPC = os.getenv("SOLANA_RPC")
 CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS")
@@ -15,6 +16,7 @@ INSTAGRAM_ACCESS_TOKEN = os.getenv("INSTAGRAM_ACCESS_TOKEN", "")
 X_API_KEY = os.getenv("X_API_KEY", "")
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "")
 
+# متغیرهای اضافه شده قبلی
 APP_DOMAIN = os.getenv("APP_DOMAIN", "https://ccoin-final.onrender.com")
 WEBHOOK_TOKEN = os.getenv("WEBHOOK_TOKEN")
 ADMIN_PRIVATE_KEY = os.getenv("ADMIN_PRIVATE_KEY")
