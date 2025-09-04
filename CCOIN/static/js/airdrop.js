4268a6403234dc65dcef987d71af4949d073e7cb
ccoin2025 <ccoin.ctg@gmail.com>
2025-09-05 00:27:00 +0330

    Update airdrop.js

--- CCOIN/static/js/airdrop.js
+++ CCOIN/static/js/airdrop.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     const endTime = new Date("2025-10-01T23:59:59Z").getTime();
     const countdown = setInterval(() => {
         const now = new Date().getTime();
         const distance = endTime - now;
-
         document.getElementById("days").innerText = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
         document.getElementById("hours").innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
         document.getElementById("minutes").innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
