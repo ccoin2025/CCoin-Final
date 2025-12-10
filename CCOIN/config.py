9b8c967aa8fa5e1953fdc8fd89fe0d42c2093faf
ccoin2025 <ccoin.ctg@gmail.com>
2025-12-10 18:51:07 +0330

    Update config.py

--- CCOIN/config.py
+++ CCOIN/config.py
@@ -18,6 +18,16 @@ X_USERNAME = os.getenv("X_USERNAME", "CCOIN_OFFICIAL")
 YOUTUBE_CHANNEL_HANDLE = os.getenv("YOUTUBE_CHANNEL_HANDLE", "@CCOIN_OFFICIAL")
 
 SOLANA_RPC = os.getenv("SOLANA_RPC", "https://api.mainnet-beta.solana.com")
+SOLANA_RPC_FALLBACK_1 = os.getenv("SOLANA_RPC_FALLBACK_1", "https://mainnet.helius-rpc.com/?api-key=")
+SOLANA_RPC_FALLBACK_2 = os.getenv("SOLANA_RPC_FALLBACK_2", "https://solana-mainnet.rpc.extrnode.com")
+
+RPC_MAX_RETRIES = int(os.getenv("RPC_MAX_RETRIES", "3"))
+RPC_RETRY_DELAY = int(os.getenv("RPC_RETRY_DELAY", "2")) 
+RPC_TIMEOUT = int(os.getenv("RPC_TIMEOUT", "30")) 
+
+TX_SCAN_LIMIT = int(os.getenv("TX_SCAN_LIMIT", "100"))  
+TX_FINALIZATION_WAIT = int(os.getenv("TX_FINALIZATION_WAIT", "10"))  
+
 ADMIN_WALLET = os.getenv("ADMIN_WALLET", "5YFFCvmi2f4ZWZYUWWBuMSmmjXrYA1QptaTaLG8vi15K")
 COMMISSION_AMOUNT = float(os.getenv("COMMISSION_AMOUNT", "0.01"))
 CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS", "TokenkegQfeZyiNwAJbNbGK7Qx6m")
@@ -29,7 +39,6 @@ if not SECRET_KEY:
     logger.warning("SECRET_KEY is not set in environment variables")
 
 CSRF_SECRET_KEY = os.getenv("CSRF_SECRET_KEY", SECRET_KEY)
-
 
 DAPP_PRIVATE_KEY = os.getenv("DAPP_PRIVATE_KEY")
 if not DAPP_PRIVATE_KEY:
@@ -51,7 +60,7 @@ if not WEBHOOK_TOKEN:
     logger.warning("WEBHOOK_TOKEN is not set in environment variables")
 
 CACHE_ENABLED = os.getenv("CACHE_ENABLED", "true").lower() == "true"
-CACHE_TTL = int(os.getenv("CACHE_TTL", "300"))  
+CACHE_TTL = int(os.getenv("CACHE_TTL", "300"))
 
 RATE_LIMIT_ENABLED = os.getenv("RATE_LIMIT_ENABLED", "true").lower() == "true"
 GLOBAL_RATE_LIMIT = os.getenv("GLOBAL_RATE_LIMIT", "100/minute")
