a905c36d6543cf9843b59a9eef830b38997b8118
ccoin2025 <ccoin.ctg@gmail.com>
2025-12-18 00:58:46 +0330

    Update database.py

--- CCOIN/database.py
+++ CCOIN/database.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 import os
 import structlog
 from CCOIN.config import BOT_USERNAME
-from CCOIN.models.transaction import Transaction
 
 logger = structlog.get_logger()
 
