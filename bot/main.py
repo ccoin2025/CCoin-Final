0403772d5598a89b155173ec24c29e92c8616350
ccoin2025 <ccoin.ctg@gmail.com>
2026-02-10 01:52:49 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,12 @@
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from anti_spam import anti_spam_handler
 
-BOT_TOKEN = "7376191947:AAEt28J_D-JKvGmAOIh6s0lNzCGdiHe1GpQ"
 
 def main():
-    app = Application.builder().token(BOT_TOKEN).build()
+   app = ApplicationBuilder().token("7376191947:AAEt28J_D-JKvGmAOIh6s0lNzCGdiHe1GpQ").build()
 
-    app.add_handler(
-        MessageHandler(filters.TEXT & ~filters.COMMAND, anti_spam_handler)
-    )
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, anti_spam_handler))
+
     print("🤖 Anti-Spam Bot is running...")
     app.run_polling()
 
